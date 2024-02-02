8562cec96096b61316e01a48ab0626134a4f9afc
victornwakpa <nwakpavictor@gmail.com>
2024-02-02 14:39:17 +0100

    change icon src url

--- app/data/icons.js
+++ app/data/icons.js
@@ -2,82 +2,82 @@ const icons = [
     {
         id: 1,
         name : "Catering",
-        src: "http://localhost:3000/assets/icons/icon-01.png",
+        src: "/assets/icons/icon-01.png",
     },
     {
         id: 2,
         name : "Childcare",
-        src: "http://localhost:3000/assets/icons/icon-02.png",
+        src: "/assets/icons/icon-02.png",
     },
     {
         id: 3,
         name : "Cleaning",
-        src: "http://localhost:3000/assets/icons/icon-03.png",
+        src: "/assets/icons/icon-03.png",
     },
     {
         id: 4,
         name : "Entertainment",
-        src: "http://localhost:3000/assets/icons/icon-04.png",
+        src: "/assets/icons/icon-04.png",
     },
     {
         id: 5,
         name : "Appliance Repair",
-        src: "http://localhost:3000/assets/icons/icon-05.png",
+        src: "/assets/icons/icon-05.png",
     },
     {
         id: 6,
         name : "Mobile Mechanic",
-        src: "http://localhost:3000/assets/icons/icon-06.png",
+        src: "/assets/icons/icon-06.png",
     },
     {
         id: 7,
         name : "Personal Assistance",
-        src: "http://localhost:3000/assets/icons/icon-07.png",
+        src: "/assets/icons/icon-07.png",
     },
     {
         id: 8,
         name : "Pet Groomers",
-        src: "http://localhost:3000/assets/icons/icon-08.png",
+        src: "/assets/icons/icon-08.png",
     },
     {
         id: 9,
         name : "Recovery Services",
-        src: "http://localhost:3000/assets/icons/icon-09.png",
+        src: "/assets/icons/icon-09.png",
     },
     {
         id: 10,
         name : "Computer & Phone Repair",
-        src: "http://localhost:3000/assets/icons/icon-10.png",
+        src: "/assets/icons/icon-10.png",
     },
     {
         id: 11,
         name : "Removal Services",
-        src: "http://localhost:3000/assets/icons/icon-11.png",
+        src: "/assets/icons/icon-11.png",
     },
     {
         id: 12,
         name : "Tradesman",
-        src: "http://localhost:3000/assets/icons/icon-12.png",
+        src: "/assets/icons/icon-12.png",
     },
     {
         id: 13,
         name : "Transport Services",
-        src: "http://localhost:3000/assets/icons/icon-13.png",
+        src: "/assets/icons/icon-13.png",
     },
     {
         id: 14,
         name : "Dry Cleaning & Laundry",
-        src: "http://localhost:3000/assets/icons/icon-14.png",
+        src: "/assets/icons/icon-14.png",
     },
     {
         id: 15,
         name : "Health, Wellness & Beauty",
-        src: "http://localhost:3000/assets/icons/icon-15.png",
+        src: "/assets/icons/icon-15.png",
     },
     {
         id: 16,
         name : "Tuiton & Classes",
-        src: "http://localhost:3000/assets/icons/icon-16.png",
+        src: "/assets/icons/icon-16.png",
     }
 ];
 
