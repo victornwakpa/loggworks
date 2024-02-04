cab30a7398fa8a9fbb0b1b1e2b532f2a74986e43
victornwakpa <nwakpavictor@gmail.com>
2024-02-04 16:45:06 +0100

    new fix

--- app/data/entertainment.js
+++ app/data/entertainment.js
@@ -2,22 +2,22 @@ const entertainment = [
     {
         id: 1,
         name : "Event planners",
-        src: "/assets/entertainment/entertainment-01.png",
+        src: "/assets/entertainment/entertainment-02.png",
     },
     {
         id: 2,
         name : "Event coverage",
-        src: "/assets/entertainment/entertainment-02.png",
+        src: "/assets/entertainment/entertainment-03.png",
     },
     {
         id: 3,
         name : "Photography & film",
-        src: "/assets/entertainment/entertainment-03.png",
+        src: "/assets/entertainment/entertainment-04.png",
     },
     {
         id: 4,
         name : "Disk Jockeys",
-        src: "/assets/entertainment/entertainment-04.png",
+        src: "/assets/entertainment/entertainment-01.png",
     },
 ];
 
