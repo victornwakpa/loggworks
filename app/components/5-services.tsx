a3d501b055fb7444e99fc5cf7b35b2030008b49a
victornwakpa <nwakpavictor@gmail.com>
2024-03-22 14:05:28 +0100

    fixed blur icons

--- app/components/5-services.tsx
+++ app/components/5-services.tsx
@@ -8,7 +8,9 @@ export default function Services() {
             <div className="grid grid-cols-8 gap-8 gap-y-8 mt-12">
                 {data.map((icon) => (
                     <div key={icon.id} className="flex flex-col gap-2 justify-center items-center">
-                        <Image src={icon.src} alt={icon.name} width={56} height={56} />
+                        <div className="rounded-[8px] p-4 border-2 border-gray-400">
+                            <Image src={icon.src} alt={icon.name} width={24} height={24} />
+                        </div>
                         <p className="text-sm text-center font-medium text-gray-10">{icon.name}</p>
                     </div>
                 ))}
--- app/data/icons.js
+++ app/data/icons.js
@@ -2,82 +2,82 @@ const icons = [
     {
         id: 1,
         name : "Catering",
-        src: "/assets/icons/icon-01.png",
+        src: "/assets/icons/catering.svg",
     },
     {
         id: 2,
         name : "Childcare",
-        src: "/assets/icons/icon-02.png",
+        src: "/assets/icons/childcare.svg",
     },
     {
         id: 3,
         name : "Cleaning",
-        src: "/assets/icons/icon-03.png",
+        src: "/assets/icons/cleaning.svg",
     },
     {
         id: 4,
         name : "Entertainment",
-        src: "/assets/icons/icon-04.png",
+        src: "/assets/icons/entertainment.svg",
     },
     {
         id: 5,
         name : "Appliance Repair",
-        src: "/assets/icons/icon-05.png",
+        src: "/assets/icons/appliance-repair.svg",
     },
     {
         id: 6,
         name : "Mobile Mechanic",
-        src: "/assets/icons/icon-06.png",
+        src: "/assets/icons/mobile-mechanic.svg",
     },
     {
         id: 7,
         name : "Personal Assistance",
-        src: "/assets/icons/icon-07.png",
+        src: "/assets/icons/personal-assistance.svg",
     },
     {
         id: 8,
         name : "Pet Groomers",
-        src: "/assets/icons/icon-08.png",
+        src: "/assets/icons/pet-groomers.svg",
     },
     {
         id: 9,
         name : "Recovery Services",
-        src: "/assets/icons/icon-09.png",
+        src: "/assets/icons/recovery-services.svg",
     },
     {
         id: 10,
         name : "Computer & Phone Repair",
-        src: "/assets/icons/icon-10.png",
+        src: "/assets/icons/computer-phone-repair.svg",
     },
     {
         id: 11,
         name : "Removal Services",
-        src: "/assets/icons/icon-11.png",
+        src: "/assets/icons/removal-services.svg",
     },
     {
         id: 12,
         name : "Tradesman",
-        src: "/assets/icons/icon-12.png",
+        src: "/assets/icons/tradesman.svg",
     },
     {
         id: 13,
         name : "Transport Services",
-        src: "/assets/icons/icon-13.png",
+        src: "/assets/icons/transport-services.svg",
     },
     {
         id: 14,
         name : "Dry Cleaning & Laundry",
-        src: "/assets/icons/icon-14.png",
+        src: "/assets/icons/dry-cleaning-laundry.svg",
     },
     {
         id: 15,
         name : "Health, Wellness & Beauty",
-        src: "/assets/icons/icon-15.png",
+        src: "/assets/icons/health-wellness-beauty.svg",
     },
     {
         id: 16,
         name : "Tuiton & Classes",
-        src: "/assets/icons/icon-16.png",
+        src: "/assets/icons/tuiton-classes.svg",
     }
 ];
 
