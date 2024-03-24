192a48813ee8cf4b46bf1aa47c2c86d92d1f390b
victornwakpa <nwakpavictor@gmail.com>
2024-03-24 17:51:48 +0100

    finished mobile testimonial

--- app/components/8-testimonial.tsx
+++ app/components/8-testimonial.tsx
@@ -1,5 +1,5 @@
 "use client";
-import trustscore from "@/public/assets/trust-score.png";
+import trustpilot from "@/public/assets/trustpilot.png";
 import Image from "next/image";
 import data from "@/app/data/data.js";
 
@@ -14,7 +14,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 export default function Testimonial() {
     return (
         <div className="bg-primary-90">
-            <div className="container px-5 py-10 lg:py-20">
+            <div className="container px-5 pt-10 pb-8 lg:py-20">
                 <h2 className="text-3xl font-bold text-center">We’re trusted by many across the UK</h2>
                 <div className=" mt-12">
                     <Swiper
@@ -54,11 +54,18 @@ export default function Testimonial() {
                             <div className="swiper-button-next slider-arrow p-1 rounded-full bg-white text-gray-800 hover:bg-white">
                                 <ChevronRight />
                             </div>
-
                         </div>
                         <div className="swiper-pagination"></div>
                     </Swiper>
 
+                    <div className="flex items-center flex-col mt-16">
+                        <Image src={trustpilot} alt="trust pilot review" className="mx-auto" width={170} height={70} />
+
+                        <div className="flex justify-between mt-2 divide-x divide-gray-500">
+                            <p className="text-[11px] px-1 ">Trustscore <span className="font-semibold">4.5</span></p>
+                            <p className="text-[11px] px-1"><span className="font-semibold">15,203</span> reviews</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
